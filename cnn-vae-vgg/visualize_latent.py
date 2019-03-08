e41222d5a98bd862bf55fd517fb7d12b5c80ade4
Ahad Shoaib <ahad_cool@hotmail.co.uk>
2019-03-08 13:44:56 -0500

    updated visualizer

--- cnn-vae-vgg/visualize_latent.py
+++ cnn-vae-vgg/visualize_latent.py
@@ -8,19 +8,23 @@ from sklearn.manifold import TSNE
 
 import pickle
 
+import sys
+
 z_mu_new = pickle.load(open("z_mu.p", "rb"))
 y_val = pickle.load(open("y_val.p", "rb"))
 
-latent_dim = 2
+pp = 30
+latent_dim = 50
 
-y_val[0] = "test"
+
+#y_val[0] = "test"
 USE_TSNE = latent_dim > 2
 
 if USE_TSNE:
-    reducer = TSNE(n_components=2, verbose=1)
+    reducer = TSNE(n_components=2, perplexity=pp,verbose=1)
     z_mu_2 = reducer.fit_transform(z_mu_new)
     plt.figure(figsize=(10, 10))
-    plt.scatter(z_mu_2[:, 0], z_mu_2[:, 1], c=y_train, cmap='brg')
+    plt.scatter(z_mu_2[:, 0], z_mu_2[:, 1], c=y_val, cmap='brg')
     plt.colorbar()
 #     plt.show()
 else:
@@ -29,4 +33,4 @@ else:
     plt.colorbar()
 #     plt.show()
     
-plt.savefig("test.png")
+plt.savefig("test-pp" + str(pp) +"-lat" + str(latent_dim) + " ".join(sys.argv[1:]) + ".png")
