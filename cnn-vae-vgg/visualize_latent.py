ade2e32f76fefd27ca03a08ceb8899481d4d5728
Ahad Shoaib <potatozero@Ahads-MacBook-Pro.local>
2019-03-18 07:55:15 -0400

    fixed vis

--- cnn-vae-vgg/visualize_latent.py
+++ cnn-vae-vgg/visualize_latent.py
@@ -10,12 +10,17 @@ import pickle
 
 import sys
 
+ll = lambda f: pickle.load(open(f, "rb"))
+
 z_mu_new = pickle.load(open("z_mu.p", "rb"))
 y_val = pickle.load(open("y_val.p", "rb"))
 artists = pickle.load(open("artists.p", "rb"))
 
+priors = ll("priors.p")
+
 pp = 30
 latent_dim = 50
+n_samples = 50 # num samples for prior
 
 art_back = {}
 for k, v in artists.items():
@@ -31,11 +36,19 @@ for i in range(max(y_val)):
         y_j = new_y_val[j]
         if y_j != j:
             new_y_val[j] = i+1
+
+    ps = []
+    for k in n_samples:
+        z_mu_new = np.append(z_mu_new, np.random.normal(priors[i][0], priors[i][1]), axis=0)
+        new_y_val = np.append(new_y_val, i+2)    
+
     if USE_TSNE:
-            if i == 0:
-                reducer = TSNE(n_components=2, perplexity=pp,verbose=1)
-                z_mu_2 = reducer.fit_transform(z_mu_new)
+        if i == 0:
+            reducer = TSNE(n_components=2, perplexity=pp,verbose=1)
+            z_mu_2 = reducer.fit_transform(z_mu_new)
         plt.figure(figsize=(10, 10))
+        x = z_mu_2[:,0]
+        y = z_mu_2[:,1]
         plt.scatter(z_mu_2[:, 0], z_mu_2[:, 1], c=new_y_val, cmap='brg')
         plt.colorbar()
     #     plt.show()
@@ -46,3 +59,5 @@ for i in range(max(y_val)):
     #     plt.show()
         
     plt.savefig(str(art_back[i]) + "-test-pp" + str(pp) +"-lat" + str(latent_dim) + " ".join(sys.argv[1:]) + ".png")
+
+
